b65b616088233aeb72d43d100909d073192ad4dd
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-04-12 00:10:26 +0530

    Made navbar responsive for mediaMd <

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,21 @@
-const content = document.querySelector('.content')
-const loadingOverlay = document.querySelector('.loading-overlay')
+const content = document.querySelector('.content');
+const loadingOverlay = document.querySelector('.loading-overlay');
+const navLinks = document.querySelector('.nav__links');
+const navToggle = document.getElementById('nav-toggle');
 
-setTimeout(()=>{
-  content.classList.add('appear')
-  loadingOverlay.classList.add('dissappear')
-}, 2000)
+setTimeout(() => {
+  content.classList.add('appear');
+  loadingOverlay.classList.add('dissappear');
+}, 2000);
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('toggled');
+  navToggle.classList.toggle('toggled');
+
+});
+
+for(let i =0; i<navLinks.children.length;i++){
+  navLinks.children[i].addEventListener('click', ()=>{
+    navLinks.classList.toggle('toggled');
+  })
+}
