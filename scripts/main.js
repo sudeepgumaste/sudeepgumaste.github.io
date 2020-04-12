1cfd48338f9a411f709ad88174d9952701f5abd2
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-04-12 21:36:55 +0530

    Added IntersectionObservers for cool stuff

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const content = document.querySelector('.content');
 const loadingOverlay = document.querySelector('.loading-overlay');
 const navLinks = document.querySelector('.nav__links');
 const navToggle = document.getElementById('nav-toggle');
+const sections = document.querySelectorAll('section')
 
 setTimeout(() => {
   content.classList.add('appear');
@@ -20,3 +21,24 @@ for(let i =0; i<navLinks.children.length;i++){
     navToggle.classList.toggle('toggled');
   })
 }
+
+const observer = new IntersectionObserver((entries, observer)=>{
+  entries.forEach(entry=>{
+    // console.log(entry.target, entry.isIntersecting);
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+      document.getElementById(`${entry.target.id}-link`).classList.add('visible')
+    }
+    else{
+      document.getElementById(`${entry.target.id}-link`).classList.remove('visible')
+      // entry.target.classList.remove('visible');
+    }
+  });
+}, {
+  // rootMargin: "0px 0px -500px 0px ",
+  threshold: 0.5
+})
+
+sections.forEach(section=>{
+  observer.observe(section)
+})
